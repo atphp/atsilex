0b161290ae84b441b51ac0651a9b61768dcaa92d
Andy Truong <thehongtt@gmail.com>
2015-08-12 05:45:30 +0700

    A script to split modules to read-only repositories.

---
+++ src/scripts/SplitSubTree.php
@@ -0,0 +1,33 @@
+<?php
+
+return call_user_func(function () {
+
+    $origin = 'git@github.com:v3knet/v3k.git';
+    $branches = ['0.1'];
+    $tmp = '/tmp';
+    $trees = [
+        'modules/swagger' => 'git@github.com:v3knet/swagger-module.git',
+        'modules/system'  => 'git@github.com:v3knet/system-module.git',
+        'modules/queue'   => 'git@github.com:v3knet/queue-module.git',
+    ];
+
+    foreach ($trees as $local => $url) {
+      foreach ($branches as $branch) {
+        passthru(implode('; ', [
+          "rm -rf $tmp/v3k",
+          "git clone $origin $tmp/v3k",
+          "cd $tmp/v3k",
+          "git filter-branch -f --prune-empty --subdirectory-filter $local $branch",
+          "git remote add read-only $url",
+          "git push -f read-only $branch",
+          "cd -",
+          "rm -rf $tmp/v3k",
+        ]));
+      }
+    }
+
+    require_once __DIR__ . '/ComposerBuildRoot.php';
+
+    passthru('composer update -vv');
+
+});
