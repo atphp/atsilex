b7910e80d1492ee77c80572b2f212bd6899b8593
Andy Truong <thehongtt@gmail.com>
2015-08-12 10:54:46 +0700

    Use config.default.php if config.php is not available.

--- public/index.php
+++ public/index.php
@@ -8,13 +8,18 @@ global $loader;
 $loader = $loader ?: require_once __DIR__ . '/../vendor/autoload.php';
 
 // Get rid of global variables
-return call_user_func(function() use ($loader) {
-  // APP_ROOT is useful to get app directories/files.
-  !defined('APP_ROOT') && define('APP_ROOT', dirname(dirname(__DIR__)));
+return call_user_func(function () use ($loader) {
+    // APP_ROOT is useful to get app directories/files.
+    !defined('APP_ROOT') && define('APP_ROOT', dirname(dirname(__DIR__)));
 
-  // app object, the core of the application.
-  $app = new App(require dirname(__DIR__) . '/config.php', $loader);
+    // app object, the core of the application.
+    $configFile = APP_ROOT . '/config.php';
+    if (!file_exists($configFile)) {
+        $configFile = APP_ROOT . '/config.default.php';
+    }
 
-  // Handle user's request or return the application object.
-  return defined('APP_CLI') ? $app : $app->run();
+    $app = new App(require $configFile, $loader);
+
+    // Handle user's request or return the application object.
+    return defined('APP_CLI') ? $app : $app->run();
 });
