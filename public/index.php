ef7061c7e6718fd53d45cb2fe36b1df5a992db33
Andy Truong <thehongtt@gmail.com>
2015-08-12 14:28:13 +0700

    Remove vendor_name\project_name\App class.

--- public/index.php
+++ public/index.php
@@ -1,14 +1,14 @@
 <?php
 
-use vendor_name\project_name\App;
-
-global $loader;
-
-// Class loader is injected to app, can be used to load module's classes.
-$loader = $loader ?: require_once __DIR__ . '/../vendor/autoload.php';
+use v3knet\module\system\ModularApp;
 
 // Get rid of global variables
-return call_user_func(function () use ($loader) {
+return call_user_func(function () {
+    global $loader;
+
+    // Class loader is injected to app, can be used to load module's classes.
+    $loader = $loader ?: require_once __DIR__ . '/../vendor/autoload.php';
+
     // APP_ROOT is useful to get app directories/files.
     !defined('APP_ROOT') && define('APP_ROOT', dirname(__DIR__));
 
@@ -18,7 +18,7 @@ return call_user_func(function () use ($loader) {
         $configFile = APP_ROOT . '/config.default.php';
     }
 
-    $app = new App(require $configFile, $loader);
+    $app = new ModularApp(require $configFile, $loader);
 
     // Handle user's request or return the application object.
     return defined('APP_CLI') ? $app : $app->run();
