f534a2205e2ec9e1013202e4d3a15e07531c098e
Andy Truong <thehongtt@gmail.com>
2015-08-12 11:47:48 +0700

    Remove useless AppTest.php

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 error_reporting(-1);
 
 // Path to app root.
-define('APP_ROOT', dirname(__DIR__) . '/');
+define('APP_ROOT', __DIR__);
 
 // User composer's class loader
-require_once __DIR__ . '/../vendor/autoload.php';
+return require_once __DIR__ . '/vendor/autoload.php';
