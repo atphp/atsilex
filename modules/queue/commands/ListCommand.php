780441b417d4dcf95ffeda2db6e0acc6925e4759
Andy T. <andytruong@users.noreply.github.com>
2015-08-16 18:10:30 +0700

    Create ListCommand.php

---
+++ modules/queue/commands/ListCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace atsilex\module\queue\commands;
+
+use atsilex\module\App;
+use atsilex\module\system\ModularApp;
+use Bernard\QueueFactory;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ListCommand extends Command
+{
+
+    /** @var ModularApp */
+    protected $app;
+
+    public function __construct(ModularApp $app)
+    {
+        $this->app = $app;
+
+        parent::__construct('v3k:queue:list');
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('Message queue consumer');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $table = new Table($output);
+        $queues = $this->app['bernard.queues'];
+        foreach ($queues->keys() as $k) {
+            $table->addRow([$k, $queues[$k]]);
+        }
+
+        $table
+            ->setHeaders(['Queue', 'Message class'])
+            ->render();
+    }
+
+}
