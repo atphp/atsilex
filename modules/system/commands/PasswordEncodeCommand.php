2518572b7a3b1db60a2c6345861229ef17c88e36
Andy T. <andytruong@users.noreply.github.com>
2015-08-19 10:13:00 +0700

    Create PasswordEncodeCommand.php

---
+++ modules/system/commands/PasswordEncodeCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace atsilex\module\system\commands;
+
+use atsilex\module\system\ModularApp;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
+
+class PasswordEncodeCommand extends Command
+{
+
+    /** @var  ModularApp */
+    protected $app;
+
+    public function __construct(ModularApp $app)
+    {
+        $this->app = $app;
+
+        parent::__construct('v3k:password-encode');
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Encode a password')
+            ->addArgument('password', InputArgument::REQUIRED)
+            ->addArgument('salt', InputArgument::OPTIONAL);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $encoder = new MessageDigestPasswordEncoder();
+        $password = $input->getArgument('password');
+        $salt = $input->getArgument('salt') ?: '';
+
+        $output->writeln('Passord: ' . $encoder->encodePassword($password, $salt));
+    }
+
+}
