e6e9684ed353e2ed10eca3a4dc77ac1009160d7c
Andy Truong <thehongtt@gmail.com>
2015-08-25 10:54:56 +0700

    misc

--- modules/system/commands/InstallerScript.php
+++ modules/system/commands/InstallerScript.php
@@ -9,9 +9,8 @@ use Composer\Script\Event;
  *
  * In your composer.json add this command to your scripts.post-install-cmd and scripts.post-update-cmd:
  *
- *  v3knet\module\system\commands\InstallerScript::execute
+ *  atsilex\module\system\commands\InstallerScript::execute
  *
- * @TODO Use PHP to create files instead of bash commands.
  */
 class InstallerScript
 {
@@ -31,38 +30,44 @@ class InstallerScript
         return $me->install($event);
     }
 
+    /**
+     * @TODO Use PHP to create files instead of bash commands.
+     * @param Event $event
+     */
     public function install(Event $event)
     {
-        // Put default files in place.
+        $cli = 'php %root/public/index.php';
+
         $this
-            // `files` directory
             ->run('rsync -a %default/files/ %root/files/')
             ->run('chmod -Rf 777 %root/files')
-            // `public` directory
             ->run('mkdir -p %root/public/assets/modules/')
             ->run('rsync %default/public/index.php %root/public/index.php')
-            // Config file
             ->run('rsync %default/config.default.php %root/config.default.php')
-            ->run('php %root/public/index.php v3k:generate-config-file')
-            // Build assets
-            ->run('php %root/public/index.php v3k:build-assets');
+            ->run("$cli v3k:generate-config-file")
+            ->run("$cli orm:schema-tool:update --force")
+            ->run("$cli v3k:build-assets")
+            ->run("$cli v3k:cache:flush");
 
         // Find and replace tokens defined in composer's extra
         $extras = $event->getComposer()->getPackage()->getExtra();
         if (isset($extras) && !empty($extras['atsilex'])) {
-            $code = file_get_contents("$this->root/config.default.php");
-            file_put_contents("$this->root/config.default.php", strtr($code, $extras['atsilex']));
+            file_put_contents(
+                "$this->root/config.default.php",
+                strtr(
+                    file_get_contents("$this->root/config.default.php"),
+                    $extras['atsilex']
+                )
+            );
         }
     }
 
     private function run($cmd)
     {
-        $cmd = strtr($cmd, [
+        passthru(strtr($cmd, [
             '%default' => dirname(__DIR__) . '/resources/default-app',
             '%root'    => $this->root
-        ]);
-
-        passthru($cmd);
+        ]));
 
         return $this;
     }
