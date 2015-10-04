0c8e2a541178f9c13ce6fddda678e87917afcf8f
Andy Truong <thehongtt@gmail.com>
2015-10-04 10:34:52 +0700

    Add v3k:scr - This command is useful we we need run some code quickly without writing any command or controller.

--- modules/system/commands/ScriptCommand.php
+++ modules/system/commands/ScriptCommand.php
@@ -3,31 +3,37 @@
 namespace atsilex\module\system\commands;
 
 use atsilex\module\system\ModularApp;
-use Boris\Boris;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ShellCommand extends Command
+class ScriptCommand extends Command
 {
     /** @var  ModularApp */
     protected $app;
 
-    /** @var  Boris */
-    protected $shell;
-
     public function __construct(ModularApp $app)
     {
-        $this->shell = $app['shell'];
+        $this->app = $app;
 
         $vendor = isset($app['vendor_machine_name']) ? $app['vendor_machine_name'] : 'v3k';
-        parent::__construct($vendor . ':shell');
+        parent::__construct($vendor . ':scr');
+    }
 
-        $this->setDescription('Start interacting shell.');
+    protected function configure()
+    {
+        $this
+            ->setDescription('Execute a PHP file')
+            ->addArgument('file', InputArgument::REQUIRED);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        return $this->shell->start();
+        if ($result = require $input->getArgument('file')) {
+            if (is_callable($result)) {
+                call_user_func($result, $this->app);
+            }
+        }
     }
 }
--- resources/docs/console.md
+++ resources/docs/console.md
@@ -1,13 +1,25 @@
 Console
 ====
 
-Start console:
+### Start console
 
     php public/index.php
 
-Start PHP REPL
+### Start PHP REPL
 
     php public/index.php v3k:shell
 
     @silex > $app;
     @silex > exit; // or control-D
+
+### Run a PHP file
+
+This command is useful we we need run some code quickly without writing any command or controller:
+
+
+    php public/index.php v3k:scr /path/to/debug.php
+
+    # @file /path/to/debug.php
+    return function(atsilex\module\system\ModularApp $app) {
+        dump($app->keys());
+    }
