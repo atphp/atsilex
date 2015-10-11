8dfa6e4b378b8b90220dd56ce38c3a689e9f101f
Andy Truong <thehongtt@gmail.com>
2015-10-11 17:35:39 +0700

    Trying to fix SplitSubTree.php

--- src/scripts/SplitSubTree.php
+++ src/scripts/SplitSubTree.php
@@ -5,40 +5,66 @@
  * -------
  */
 
-if (false !== strpos(phpversion(), '5.6.')) {
-    return call_user_func(function () {
-        $origin = 'git@github.com:atphp/atsilex.git'; # The main repository
-        $branches = ['0.1', '0.2'];                   # Only push main branches
-        $tmp = '/tmp';                                # Temp dir
-        $trees = [
-            'src/module'      => 'git@github.com:v3knet/module.git',
-            'modules/dev'     => 'git@github.com:v3knet/dev-module.git',
-            'modules/orm'     => 'git@github.com:v3knet/orm-module.git',
-            'modules/queue'   => 'git@github.com:v3knet/queue-module.git',
-            'modules/swagger' => 'git@github.com:v3knet/swagger-module.git',
-            'modules/system'  => 'git@github.com:v3knet/system-module.git',
-        ];
+namespace atsilex\scripts;
 
-        if ($privateKeyUrl = getenv('PRIVATE_KEY_URL')) {
-            passthru("touch ~/.ssh/id_rsa");
-            $data = file_get_contents($privateKeyUrl);
-            file_put_contents('/home/travis/.ssh/id_rsa', $data);
-            passthru("chmod 600 ~/.ssh/id_rsa");
-        }
+class SplitSubTree
+{
+    private $privateKeyUrl;
+    private $origin   = 'git@github.com:atphp/atsilex.git';
+    private $branches = ['0.3'];
+    private $trees    = [
+        'src/module'      => 'git@github.com:v3knet/module.git',
+        'modules/dev'     => 'git@github.com:v3knet/dev-module.git',
+        'modules/orm'     => 'git@github.com:v3knet/orm-module.git',
+        'modules/queue'   => 'git@github.com:v3knet/queue-module.git',
+        'modules/swagger' => 'git@github.com:v3knet/swagger-module.git',
+        'modules/system'  => 'git@github.com:v3knet/system-module.git',
+    ];
 
-        foreach ($trees as $local => $url) {
-            foreach ($branches as $branch) {
-                passthru(implode('; ', [
-                    "rm -rf $tmp/v3k",
-                    "git clone -q --single-branch --branch=$branch $origin $tmp/v3k",
-                    "cd $tmp/v3k",
-                    "git filter-branch -f --prune-empty --subdirectory-filter $local $branch",
-                    "git remote add read-only $url",
-                    "git push -q -f --tags read-only $branch",
-                    "cd -",
-                    "rm -rf $tmp/v3k",
-                ]));
+    /**
+     * SplitSubTree constructor.
+     */
+    public function __construct()
+    {
+        $this->privateKeyUrl = getenv('PRIVATE_KEY_URL');
+    }
+
+    public function execute()
+    {
+        $this->configureKeys();
+
+        foreach ($this->trees as $local => $url) {
+            foreach ($this->branches as $branch) {
+                $this->split($local, $branch, $url);
             }
         }
+    }
+
+    private function configureKeys()
+    {
+        passthru("touch ~/.ssh/id_rsa");
+        $data = file_get_contents($this->privateKeyUrl);
+        file_put_contents('/home/travis/.ssh/id_rsa', $data);
+        passthru("chmod 600 ~/.ssh/id_rsa");
+    }
+
+    private function split($local, $branch, $url, $tmp = '/tmp')
+    {
+        passthru(implode('; ', [
+            "rm -rf $tmp/v3k",
+            "git clone -q --single-branch --branch=$branch $this->origin $tmp/v3k",
+            "cd $tmp/v3k",
+            "git filter-branch -f --prune-empty --subdirectory-filter $local $branch",
+            "git remote add read-only $url",
+            "git push -q -f --tags read-only $branch",
+            "cd -",
+            "rm -rf $tmp/v3k",
+        ]));
+    }
+}
+
+if (false !== strpos(phpversion(), '5.6.')) {
+    return call_user_func(function () {
+        return (new SplitSubTree())->execute();
     });
 }
